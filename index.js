2af6a7b399fa1d15d1816fd87f57ddd8db6779ca
Isaiah Ware <Isaiah.ware0510@gmail.com>
2022-12-30 21:35:54 -0700

    commit tree 0b21d22fae566ed52c79b07c5d46d29ef75f2f45

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1
+}
+
+function decrement(n) {
+    return n - 1
+}
+
+function makeInt(n) {
+    parseInt(n , 10);
+    return n
+}
+
+function preserveDecimal(n) {
+    parseFloat(2.222 , 10);
+    return 2.222
+}
